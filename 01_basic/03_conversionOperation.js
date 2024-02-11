ccb1f35c9e1d0d281772e4d3dc325586af4ce067
Rupesh Virani <54779977+Ru28@users.noreply.github.com>
2024-02-11 10:02:23 +0000

    conersion of data type

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,26 @@
+// let score= "33abc";
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber = Number(score)
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; False =>0;
+
+// let isLoggedIn="";
+
+// let booleanLoggedIn= Boolean(isLoggedIn);
+// console.log(booleanLoggedIn);
+
+// 1=> true 0 => false
+// "" => false
+// "Rupesh" => true
+
+let someNumber = 33;
+let stringNumber= String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
