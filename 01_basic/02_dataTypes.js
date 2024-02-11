e8cc3414989c2fc299cd0a60defd4040a72b7f32
Rupesh Virani <54779977+Ru28@users.noreply.github.com>
2024-02-11 07:31:21 +0000

    dataTypes

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,27 @@
+"use strict"; // treat all JS code as newer version
+
+// alert (3+3); // We are using node.js, not brower
+
+console.log(6 + 
+    6); // code readbility should be high
+
+let name= "Rupesh"
+let age=18
+let isLoggedIn= false;
+let State;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined 
+// symbol => unique
+
+// object
+
+console.log(typeof "Rupesh");
+console.log(typeof null); // type object
+console.log(typeof undefined) // type undefined
+
+// console.log("Rupesh");
