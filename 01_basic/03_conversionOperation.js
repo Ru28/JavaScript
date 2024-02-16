f31283a9dd320b20106b2acd2a01c97ebf10b741
Rupesh Virani <54779977+Ru28@users.noreply.github.com>
2024-02-16 17:24:23 +0000

    operations

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -20,7 +20,38 @@
 // "" => false
 // "Rupesh" => true
 
-let someNumber = 33;
-let stringNumber= String(someNumber);
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let someNumber = 33;
+// let stringNumber= String(someNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+let value = 3
+let negValue = -value
+
+// console.log(negValue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+// let str1= "hello";
+// let str2= " Rupesh";
+// let str3= str1+str2;
+// console.log(str3);
+
+// console.log(1 + "2");
+// console.log("1" + 2);
+// console.log("1" + 2 +2);
+// console.log(1+2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+// let num1, num2, num3 ;
+// num1=num2=num3 =4;
+
+let gameCounter = 100;
+++ gameCounter;
+console.log(gameCounter);
