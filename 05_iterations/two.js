ab805551e2e4a0d6f78309b3a3a60e1b0afae5b3
Ru28 <rupeshvirani01@gmail.com>
2024-03-09 19:44:13 +0530

    while and do loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,20 @@
+let index =0;
+
+while(index<=10){
+  // console.log(`Value of index is ${index}`);
+  index = index + 2;
+}
+
+let myArray = ['flash', 'batman', 'superman']
+
+// let ind=0;
+// while (ind < myArray.length) {
+//     console.log(myArray[ind]);
+//     ind++;
+// }
+
+let score=11;
+do {
+  console.log(`Score is ${score}`);
+  score++;
+}while(score<=10);
