1809542c9079a518c97a01d9c26cb3710da29984
Rupesh Virani <rupeshvirani01@gmail.com>
2024-04-01 19:35:47 +0530

    closures in JS

---
+++ 12_JS_with_AK/closures/index.js
@@ -0,0 +1,22 @@
+// function x(){
+//     let a = 7;
+//     return function y(){
+//         console.log(a);
+//     }
+// }
+// let z= x();
+// console.log(z);
+
+// z();
+
+// //+++++++++++++++++++++++++++++++++
+
+// Uses of Closures:
+// - Module Design Pattern
+// - Currying
+// - Functions like once
+// - maintaining state in async world
+// - setTimeouts
+// - Iterators
+// - and many more...
+
